0f01bb2bf30a4bd783648660f471fc508648ada9
murraycadzow <murray.cadzow@otago.ac.nz>
2016-11-29 22:33:15 +1300

    rearrangement of data loading and manipulation

--- server.R
+++ server.R
@@ -16,84 +16,74 @@ options(shiny.maxRequestSize = 40 * 1024 ^2) # 40 Mb file limit
 shinyServer(function(input, output) {
   output$fn <-renderText({if(!is.null(input$file)){paste("You have selected:",input$file[1])}})
   
+  #load and process data
   myData <- reactive({
     if(is.null(input$file)) return(NULL)
-    
     inFile <- input$file
-    vcf <- read.table(inFile$datapath, sep = '\t', header = FALSE)
-    return(vcf)
-  })
-  
-  gt_mat <- reactive({
-    vcf<-myData()
-    if(is.null(vcf)) return(NULL)
-    gtcols <- 10:ncol(vcf)
-    
-    gt <- apply(vcf[,gtcols],1, function(x){
-      y <- unlist(sapply(x, function(x){strsplit(x, ':')[[1]][1]}))
-      return(y)
-    } 
+    vcf <- read.table(inFile$datapath, sep = '\t', header = FALSE, stringsAsFactors = FALSE)
+    return(
+      data.frame(
+        t(
+          apply(vcf, 1, function(x){
+            gt<-unlist(
+              sapply(x[10:length(x)],function(y){
+                strsplit(x = y, split = ":")[[1]][1]} #pull out genotypes
+              )
+            )
+            n <- 2 * length(gt)
+            gt[gt == './.'] <- NA # replace missing genotypes with NA
+            a <- as.numeric(
+              unlist(
+                sapply(gt[!is.na(gt)],function(y){
+                  c(substring(y, 1,1), substring(y,3,3)) # pull out alleles
+                }
+                )
+              )
+            )
+            
+            alt <- sum(a > 0)
+            return(c(chr = as.numeric(x[1]),pos = as.numeric(x[2]),af = alt/n))
+          }
+          )
+        )
+      )
     )
-    
-    l <-levels(as.factor(gt))
-    names(l) <- l
-    l[grep('./.',l)] <- NA
-    wt <- which(l == '0|0')
-    l[!is.na(l)] <- 1 #0:(length(l[!is.na(l)])-1)
-    l[wt] <- 0
-    
-    
-    gt2 <- apply(gt, 1, function(x){
-      for(n in names(l)){
-        cols <- which(x == n)
-        x[cols]<- l[which(names(l) == n)]
-      }
-      return(x)
-    })
-    gt2 <- apply(gt2,1,as.numeric)
-    return(gt2)
   })
   
+  # adjust strand
   create_freq <- reactive({
-    gt_mat <- gt_mat()
     vcf <- myData()
-    if(is.null(gt_mat) | is.null(vcf)) return(NULL)
-    
-    posSum <- apply(gt_mat, 2, function(x){sum(x, na.rm=TRUE)}) # sum rows
-    freq <- posSum/ncol(gt_mat) *100
+    if(is.null(vcf)) return(NULL)
     
     if(input$strand == '-'){
-      pos <- as.numeric(vcf$V2) * -1 
+      pos <- vcf$pos * -1 
     } else {
-      pos <- as.numeric(vcf$V2)
+      pos <- vcf$pos
     }
-    return(data.frame(freq = freq, pos = pos))
+    return(data.frame(chr = vcf$chr, freq = vcf$af * 100, pos = pos))
   })
- 
+  
+  # setup plot
   create_plot <- reactive({
     df <- create_freq()
     if(is.null(df)) return(NULL)
     p <- ggplot(df, aes(x = pos, y = freq)) +
-     geom_line() + ylim(c(0,100)) + xlab("Position (bp)") + ylab("Percentage Samples with SNP (%)") + theme_bw()
+      geom_line() + ylim(c(0,100)) + xlab("Position (bp)") + ylab("Percentage Samples with SNP (%)") + theme_bw()
     return(p)
   }) 
   
+  # render plot
   output$distPlot <- renderPlot({
-    
     p <- create_plot()
     df <- filtered()
     if(is.null(p)) return(NULL)
     if(!is.null(df) & !is.null(input$window) & input$window >= 10){
-      # if(input$strand == '-'){
-      #   df$pos <- df$pos * -1
-      #   df$end <- df$pos * -1
-      # }
       p <- p + geom_rect(data = df, aes(xmin= pos,xmax = end, ymin = -Inf, ymax = Inf), colour = 'grey10', alpha = 0.1, linetype = 0, inherit.aes = FALSE)  
     }
     p
   })
-
   
+  # filter regions
   filtered <- reactive({
     df <- create_freq()
     if(is.null(df)) return(NULL)
@@ -104,9 +94,11 @@ shinyServer(function(input, output) {
       filter(abs(diff) >= input$window)
   })
   
+  # create table of regions
   output$results <- renderTable(digits = 0,expr = {
     f <- filtered()
     if(is.null(f)) return(NULL)
+    f$diff <- abs(f$diff)
     f
   })
   
